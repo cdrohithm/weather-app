40eab3595b8ba7ee614332df0c2af2d3cc424a21
Rohith <cdrohithm@gmail.com>
2022-02-27 18:18:15 +0530

    Add icon to page

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
-
+import navigation from "./navigation.png";
 import "./Search.css";
 import WeatherContext from "../../context/WeatherContext";
 
@@ -36,8 +36,31 @@ function Search(props) {
         const { value } = e.target;
         setCity(value)
     }
+    const handleLocation = () => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let lat = position?.coords?.latitude;
+            let long = position?.coords?.longitude;
+            if (lat && long) {
+                axios
+                .get('https://weather-webapp-server.herokuapp.com/weather/' + lat + '/' + long)
+                .then(res => {
+                    console.log(res?.data)
+                    setState({ ...state, weather: res?.data });
+                    const city = res?.data?.name;
+                    history('/weather/' + city);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                    alert(err?.response?.data?.message || err?.message)
+                });
+            }
+        });
+    }
     return (
         <div className="search">
+            <div className="current" onClick={handleLocation}>
+                <img height="18" width="18" src={navigation} />
+            </div>
             <input 
                 type="text"
                 value={city}
